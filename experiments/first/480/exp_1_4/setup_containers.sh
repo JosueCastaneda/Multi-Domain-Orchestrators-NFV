28482d29ab2a0563f28c25c61ea47886d24509bd
Josue Castaneda-Cisneros <jcastane@miata.laas.fr>
2019-11-03 16:46:27 +0100

    Added destroy containers in other file for ease of use

--- experiments/first/480/exp_1_4/setup_containers.sh
+++ experiments/first/480/exp_1_4/setup_containers.sh
@@ -32,31 +32,3 @@ vim-emu compute start -d dc1 -n vnf6_mirror_x --image vnf_experiment --net '(id=
 vim-emu compute start -d dc2 -n vnf10_invert_colors --image vnf_experiment --net '(id=input,ip=10.0.0.21/24)'
 vim-emu compute start -d dc1 -n vnf8_fade_out --image vnf_experiment --net '(id=input,ip=10.0.0.19/24)'
 
-# Destroy all
-vim-emu compute stop -d dc1 -n orch_1
-vim-emu compute stop -d dc2 -n orch_2
-vim-emu compute stop -d dc1 -n orch_3
-vim-emu compute stop -d dc2 -n orch_4
-
-vim-emu compute stop -d dc1 -n source
-vim-emu compute stop -d dc1 -n vnf2_rotate
-vim-emu compute stop -d dc1 -n vnf1_crop
-vim-emu compute stop -d dc2 -n vnf5_painting
-vim-emu compute stop -d dc1 -n vnf3_resize
-vim-emu compute stop -d dc2 -n vnf9_black_white
-vim-emu compute stop -d dc1 -n vnf7_annotate
-vim-emu compute stop -d dc1 -n vnf6_mirror_x
-vim-emu compute stop -d dc2 -n vnf10_invert_colors
-vim-emu compute stop -d dc1 -n vnf8_fade_out
-
-#vim-emu compute stop -d dc1 -n source
-#vim-emu compute stop -d dc1 -n vnf2_rotate
-#vim-emu compute stop -d dc1 -n vnf1_crop
-#vim-emu compute stop -d dc2 -n vnf5_painting
-#vim-emu compute stop -d dc1 -n vnf3_resize
-#vim-emu compute stop -d dc4 -n vnf9_black_white
-#vim-emu compute stop -d dc1 -n vnf7_annotate
-#vim-emu compute stop -d dc1 -n vnf6_mirror_x
-#vim-emu compute stop -d dc2 -n vnf10_invert_colors
-#vim-emu compute stop -d dc1 -n vnf8_fade_out
-
