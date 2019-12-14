0ee6f4ddf46fdfcd0dd121b8823f6b62bbe9eaba
Josue Castaneda-Cisneros <jcastane@miata.laas.fr>
2019-12-14 21:22:36 +0100

    Debugging updating messages

--- communication_entities/messages/request_update_from_orchestrator_message.py
+++ communication_entities/messages/request_update_from_orchestrator_message.py
@@ -1,7 +1,7 @@
 import threading
 import random
 from os import wait
-
+import time
 from communication_entities.messages.abstract_message import AbstractMessage
 from communication_entities.messages.migration_ack_message import MigrationAckMessage
 from utilities.logger import log
@@ -13,6 +13,7 @@ class RequestUpdateFromOrchestratorMessage(AbstractMessage):
         super().__init__(None)
         self.current_server = None
         self.save_client_socket = None
+        self.seed = 0
 
     def answer_back(self):
         m = MigrationAckMessage(None)
@@ -20,8 +21,8 @@ class RequestUpdateFromOrchestratorMessage(AbstractMessage):
         self.current_server.send_message_to_socket(self.client_socket, m)
 
     def process_by_command_line(self):
-        wait_period = random.randint(0, 10)
-        wait(wait_period)
+        wait_period = random.uniform(0, 3)
+        time.sleep(wait_period)
         self.answer_back()
         # t = threading.Timer(wait_period, self.answer_back(), [])
         # t.start()
