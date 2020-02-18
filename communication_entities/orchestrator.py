7af7738c8c6ea6fff7be07325bd9db2ab26bc5a9
Josue Castaneda-Cisneros <jcastane@miata.laas.fr>
2020-02-18 21:04:49 +0100

    Fix str + number error

--- communication_entities/orchestrator.py
+++ communication_entities/orchestrator.py
@@ -133,7 +133,7 @@ class Orchestrator:
 
     def update_vnf_info_timer(self, service_index, vnf_index_to_change, value_to_change, new_value, clock, wait_period, name_vnf_to_update):
         wait_period += random.randint(0, 10)
-        str_log_wait = 'Waiting period: ' + wait_period
+        str_log_wait = 'Waiting period: ' + str(wait_period)
         log.info(str_log_wait)
 
         t = threading.Timer(wait_period, self.update_vnf_info, [service_index,
