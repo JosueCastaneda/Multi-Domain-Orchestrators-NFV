c934115fa28d964e6fa367bbc4e9fad58fefeca8
Josue Castaneda-Cisneros <jcastane@miata.laas.fr>
2020-07-28 21:54:14 +0200

    Added clean to normal

--- run_normal.sh
+++ run_normal.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Clean in case something was running
+./clean.sh
+
 # This script launches an orchestrator based on the auto-generated experiments
 # In the case of external use (cloud) this is the main file to run orchestrators
 
