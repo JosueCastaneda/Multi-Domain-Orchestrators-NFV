c484523ba1b86bea887ac631a59ef335138b9338
jcastane <josue.castaneda.cisne@gmail.com>
2021-05-12 10:10:25 +0200

    Remove debug

--- communication_entities/orchestrator.py
+++ communication_entities/orchestrator.py
@@ -260,11 +260,11 @@ class Orchestrator:
         logging.basicConfig(filename=other_folder)
         self.log = logging.getLogger('logger')
         self.log.propagate = False
-        self.log.setLevel(logging.DEBUG)
+        self.log.setLevel(logging.WARNING)
         log_str = '%(asctime)s - %(filename)s - %(lineno)s - %(message)s'
         formatter = logging.Formatter(log_str)
         fh = logging.FileHandler(other_folder, mode='w', encoding='utf-8')
-        fh.setLevel(logging.DEBUG)
+        fh.setLevel(logging.WARNING)
         fh.setFormatter(formatter)
         self.log.addHandler(fh)
         ch = logging.StreamHandler()
