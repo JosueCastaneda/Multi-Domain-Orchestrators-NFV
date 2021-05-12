a43e89f7be132204cb0f194d2a58b8fad42b7430
Josue Castaneda <jc.derivado.integral@gmail.com>
2021-05-12 14:23:51 +0200

    Added sleep time between run to let the servers answer

--- test_asynchronous_vnffg_updates_local.sh
+++ test_asynchronous_vnffg_updates_local.sh
@@ -7,7 +7,11 @@ FOURTH_SERVER=127.0.0.1
 FIFTH_SERVER=127.0.0.1
 
 python3 message_factory.py --type do_asynchronous_updates --host $FIRST_SERVER --port 4437 &
+sleep 3
 python3 message_factory.py --type do_asynchronous_updates --host $SECOND_SERVER --port 4439 &
+sleep 3
 python3 message_factory.py --type do_asynchronous_updates --host $THIRD_SERVER --port 4441 &
+sleep 3
 python3 message_factory.py --type do_asynchronous_updates --host $FOURTH_SERVER --port 4443 &
+sleep 3
 python3 message_factory.py --type do_asynchronous_updates --host $FIFTH_SERVER --port 4445 &
